007fa524d9eea97d5a3d4980f2f74e9182c999b8
Simon Sawert <simon@sawert.se>
2021-06-16 23:22:42 +0200

    Clarify python example

--- python/main.py
+++ python/main.py
@@ -2,20 +2,37 @@ import asyncio
 
 
 async def async_main():
+    # Will create hot tasks that starts executing immediately.
     t1 = asyncio.create_task(dance())
     t2 = asyncio.create_task(learn_and_sing())
 
     print("waiting to learn the song, sing it and daaance!")
 
+    # We await both tasks to ensure their completion, however since we know that
+    # t1 will always run longer than t2 it would be enough to await t1 to have
+    # both executed until done. This is however somewhat more clear.
     await t1
+    await t2
+
     print("done dancing")
 
-    await t2
+
+async def async_main_gather():
+    print("waiting to learn the song, sing it and daaance!")
+
+    # An alternative way would be to just gather all the tasks and ensure
+    # they're finished by blocking until all are done.
+    await asyncio.gather(
+        dance(),
+        learn_and_sing(),
+    )
+
+    print("done dancing")
 
 
 async def learn_and_sing():
     song = await learn_song()
-    await sing_song(song)
+    sing_song(song)
 
 
 async def learn_song():
@@ -23,7 +40,7 @@ async def learn_song():
     return "this is the song"
 
 
-async def sing_song(song):
+def sing_song(song):
     print(song)
 
 
